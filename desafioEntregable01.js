ac548ed9944780b1f74dcfdab35ea441f922c9ab
dariomiguel <101842755+dariomiguel@users.noreply.github.com>
2023-09-30 11:41:58 -0300

    Corregir código para mejorar legilidad y mantenimiento _Ahora la función isNotValideCode verifica que están todos los parametros antes de cargar el producto. _Se corregío el if dentro de la función addProduct para mejorar la legibilidad.

--- desafioEntregable01.js
+++ desafioEntregable01.js
@@ -26,25 +26,34 @@ class ProductManager {
 
     //Se crea el método para agregar productos validando previamente.
     addProduct(title, description, price, thumbnail, code, stock){
+        if (this.isNotValidCode(title, description, price, thumbnail, code, stock)){
+            console.error("Atención: Verifique que todos los datos se hayan cargado correctamente o que el código no se repita!"); 
+            return
+        } //Antes de agregar verifica si es válido o no
 
-        const notValid = this.isNotValidCode(code);//Antes de agregar verifica si es valido o no
-        
-        notValid ? //Si no es valido arroja un error, si es valido la agrega a la lista.
-            console.error("Atención: El código se repite!") : 
-            this.add(title, description, price, thumbnail, code, stock);
+        //Si no es válido arroja un error, si es válido la agrega a la lista.
+        this.add(title, description, price, thumbnail, code, stock);
+        console.log(this.products); 
     }
 
-    //Validación para verificar que el código no se repita.
-    isNotValidCode = (code) => this.products.some((product) => product.code === code)
+    //Validación para verificar que el código no se repita o que no se hayan cargado todos los datos.
+    isNotValidCode = (title, description, price, thumbnail, code, stock) => { 
+        //Verificamos que existe un codigo con el mismo nombre.
+        const checker = this.products.some((product) => product.code === code);
+        //Verificamos que esten todos los productos en la carga de datos.
+        const someValid = !title || !description || !price || !thumbnail || !code || !stock;
+
+        return checker || someValid;
+    }
 
     //Verificación si existe un producto con el ID
-        getProductById = (id) => {
-            const product = this.products.find((product) => product.id == id);
-    
-            product ?
-            console.log(product):
-            console.log(`No hay un producto con el número de ID ${id}.`)
-        }
+    getProductById = (id) => {
+        const product = this.products.find((product) => product.id == id);
+
+        product ?
+        console.log(product):
+        console.log(`No hay un producto con el número de ID ${id}.`)
+    }
 }
 
 
@@ -56,6 +65,7 @@ const manager = new ProductManager();
 
 //2_Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
 manager.getProducts();
+console.log("---------------------------------------")
 
 /*3_Se llamará al método “addProduct” con los campos:
 title: “producto prueba”
@@ -69,9 +79,12 @@ manager.addProduct("producto prueba","Este es un producto prueba",200,"Sin image
 //4_El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
 //5_Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
 manager.getProducts();
+console.log("---------------------------------------")
 
 //6_Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
 manager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
+console.log("---------------------------------------")
+
 
 //7_Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
-manager.getProductById(2)
+manager.getProductById(2);
