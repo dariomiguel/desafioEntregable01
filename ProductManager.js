02cb4def263f1e78127f791b1e1364d6fac183de
dariomiguel <101842755+dariomiguel@users.noreply.github.com>
2023-10-09 23:09:30 -0300

    Entrega Desafio Clase 04

--- ProductManager.js
+++ ProductManager.js
@@ -162,67 +162,49 @@ class ProductManager {
 
 
 /*Consignas
-DESAFÍO ENTREGABLE - PROCESO DE TESTING*
+DESAFÍO ENTREGABLE - PROCESO DE TESTING*/
 
 //1_Se creará una instancia de la clase “ProductManager”
 const manager = new ProductManager();
 
-//2_Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
-manager.getProducts();
-console.log("---------------------------------------")
-
-//3_Se llamará al método “addProduct” con los campos:
-//title: “producto prueba”
-//description:”Este es un producto prueba”
-//price:200,
-//thumbnail:”Sin imagen”
-//code:”abc123”,
-//stock:25
-//manager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
-//4_El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
-//5_Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
-manager.getProducts();
-console.log("---------------------------------------")
-
-//6_Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
-manager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
-console.log("---------------------------------------")
-
-manager.addProduct("a","A","A","AAAA","a",3123123);
-//4_El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
-//5_Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
-manager.getProducts();
-console.log("---------------------------------------")
-
-//7_Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
-manager.getProductById(1);
-*/
-
-// const manager = new ProductManager();
-// manager.getProducts();
-// manager.addProduct("a","A","A","AAAA","a",3123123);
-
-const manager = new ProductManager("./database.json");
-
+//Se crea una función asincronica que siga el orden necesitado
 (async () => {
-    // await manager.getProducts();
-    // await manager.addProduct("Camiseta de algodón", "Camiseta cómoda y transpirable", 19.99, "camiseta.jpg", "SKU123",50);
-    // await manager.getProducts();
+    //2_Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
+    await manager.getProducts();
+
+    //3_Se llamará al método “addProduct” con los campos:
+    //title: “producto prueba”
+    //description:”Este es un producto prueba”
+    //price:200,
+    //thumbnail:”Sin imagen”
+    //code:”abc123”,
+    //stock:25
+    //manager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
+    //4_El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
+    await manager.addProduct("Camiseta de algodón", "Camiseta cómoda y transpirable", 19.99, "camiseta.jpg", "SKU123",50);
+    await manager.addProduct("Camiseta de algodón", "Camiseta cómoda y transpirable", 19.99, "camiseta.jpg", "SKU123",50);
+
+    //5_Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
+    await manager.getProducts();
+
+    //6_Se llamará al método “getProductById” y se corroborará que devuelva el producto con el id especificado, en caso de no existir, debe arrojar un error.
+    await manager.getProductById(0);
+
+    //7	Se llamará al método “updateProduct” y se intentará cambiar un campo de algún producto, se evaluará que no se elimine el id y que sí se haya hecho la actualización.
+    await manager.updateProduct(0, "title", "150 pokemon");
+
+    //8 Se llamará al método “deleteProduct”, se evaluará que realmente se elimine el producto o que arroje un error en caso de no existir.
+    await manager.deleteProduct(2);
+
+    //Más productos de prueba
     // await manager.addProduct("Zapatillas deportivas", "Zapatillas ideales para hacer ejercicio", 29.99, "zapatillas.jpg", "SKU456",30);
-    // await manager.getProducts();
     // await manager.addProduct("Bolso de cuero", "Bolso elegante de cuero genuino", 39.99, "bolso.jpg", "SKU789", 20);
-    // await manager.getProducts();
     // await manager.addProduct("Tablet Android", "Tablet con sistema operativo Android", 199.99, "tablet.jpg", "SKU012", 10);
-    // await manager.getProducts();
     // await manager.addProduct("Cámara digital", "Cámara de alta resolución para fotografía", 299.99, "camara.jpg", "SKU345", 5);
-    // await manager.getProducts();
-    // await manager.getProductById(3);
-    // await manager.updateProduct(3, "title", "150 pokemon");
     // await manager.getProductById(3);
     // await manager.addProduct("h", "h", 0.99, "h", "h", 5);
     // await manager.addProduct("a", "a", 0.99, "a", "a", 5);
-    // await manager.getProducts();
     // await manager.addProduct("hola", "hola", 0.99, "hola", "hola", 5);
-    await manager.deleteProduct(7);
-    await manager.getProducts();
+    
+    // await manager.getProducts();
 })();
