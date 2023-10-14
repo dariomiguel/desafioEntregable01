5d5a546b0a12c988294aa899118d64fa7e1ac324
dariomiguel <101842755+dariomiguel@users.noreply.github.com>
2023-10-14 11:55:52 -0300

    Corregir según recomendaciones de tutor _Se agregó en el constructor getProducts, para tomar los valores de la base de datos. _En getProducts modificó el modo de verificar si existe lectura. _Se eliminó el método getProductsArray y se unificó con getProducts. _Se eliminó la lógica de agregar con un writeFileSync de addProduct. _Se cambio la lógica de getProductsById para que devuelva el producto para poder reutilizarlo en otros métodos, además si el id no es encontrado devuelve un string indicando que no se encontró. _Ahora los métodos updateProduct y deleteProduct tomán el getProductById, si es un string el valor de product haran un console.log indicando que no se encontró el Id de otro modo usarán el product en su función. Se agregaro en los métodos existentes la función try/catch.

--- ProductManager.js
+++ ProductManager.js
@@ -1,4 +1,5 @@
 //Importamos el módulo para interactuar con archivos
+const { log } = require('console');
 const fs = require('fs')
 
 //Creamos la clase ProductManager que contendra los productos y metodos que necesitemos para la actividad.
@@ -6,51 +7,38 @@ class ProductManager {
 
     //Se construye el elemento inicial (un array vacío).
     constructor(path) {
-        this.products = [];
+        this.products = this.getProducts() || [];
         this.path = path;
 
         this.counter = 0;
+
     }
 
     //Se crea el retorno de los productos ingresados en el archivo database.json .
-    getProductsArray = async () => {
+    getProducts = async () => {
         //Verificamos que exista el archivo antes de leerlo
         try {
-            if (!fs.existsSync(this.path)) {
-                return this.products;
-            }
+            if (!fs.existsSync(this.path)) return [];
+
             const lectura = await fs.promises.readFile(this.path, "utf-8");
+            if(!lectura) return this.products; 
 
-            if(lectura === ""){
-                return this.products;
-            }
+            return  this.products = JSON.parse(lectura) || [];
 
-            this.products = JSON.parse(lectura);
-            return this.products;
         } catch (error) {
             console.log("Hubo un error en el READ", error);
             throw error;
         }
     }
 
-    //Método para ver en pantalla los productos ingresados
-    getProducts = async () =>{
-        if (!fs.existsSync(this.path) || fs.readFileSync(this.path, "utf-8") == "") {
-            return console.log(this.products);
-        }
-        console.log(await this.getProductsArray());
-    }
-
     //Se crea el método para agregar productos validando previamente.
     addProduct = async (title, description, price, thumbnail, code, stock) =>  {
 
         try{
+            if (!fs.existsSync(this.path)) await fs.promises.writeFile(this.path, JSON.stringify([]), "utf-8");;
             //Antes de agregar verifica si es válido o no
-            if (this.isNotValidCode(title, description, price, thumbnail, code, stock)) {
+            if (await this.isNotValidCode(title, description, price, thumbnail, code, stock)) {
                 return console.log("Atención: Verifique que todos los datos se hayan cargado correctamente o que el código de producto no se repita!");
-            //Si no existe el archivo se crea uno
-            } else if (!fs.existsSync(this.path) || fs.readFileSync(this.path, "utf-8") == "") {
-                fs.writeFileSync(this.path, JSON.stringify(this.products, null, "\t"));
             }
 
             //Si es válido la agrega al array de lista de productos.
@@ -82,7 +70,8 @@ class ProductManager {
     }
 
     //Validación para verificar que el código no se repita o que no se hayan cargado todos los datos.
-    isNotValidCode = (title, description, price, thumbnail, code, stock) => {
+    isNotValidCode = async(title, description, price, thumbnail, code, stock) => {
+        this.products = await this.getProducts();
         //Verificamos que existe un codigo con el mismo nombre.
         const checker = this.products.some((product) => product.code === code);
         //Verificamos que esten todos los productos en la carga de datos.
@@ -93,23 +82,20 @@ class ProductManager {
 
     //Verificación si existe un producto con el ID
     getProductById = async(id) => {
-        const product = await this.searchById(id);
-        product ?
-            console.log(product) :
-            console.log(`No hay un producto con el número de ID ${id}.`)
-    }
-    
-    searchById = async(id) => {
-        this.products = await this.getProductsArray();
+        this.products = await this.getProducts();
         const product = this.products.find((product) => product.id == id);
+
+        if(!product) return `No hay un producto con el número de ID ${id}.`
         return product
     }
 
     //Método para buscar un ID especificado, con la clave y el valor a actualizar 
     updateProduct = async(id, key, newValue) =>{
-        const product = await this.searchById(id);
-        if(product){
-            this.products = await this.getProductsArray();
+        try{
+            const product = await this.getProductById(id);
+            if(typeof product === "string") return console.log(product);
+
+            this.products = await this.getProducts();
             //Buscamos en que indice el id coincide
             const indice = this.products.findIndex((objeto) => objeto.id === id);
 
@@ -118,8 +104,9 @@ class ProductManager {
 
             const data = JSON.stringify(this.products, null, "\t");
             await fs.promises.writeFile(this.path, data, "utf-8");
-        }else{
-            console.log(`No hay un producto con el número de ID ${id}.`)
+            
+        } catch (error) {
+            console.log("Hubo un error en el proceso de actualización:", error);
         }
     }
 
@@ -130,7 +117,6 @@ class ProductManager {
         } else {
             // Obtener el ID más grande del array de productos
             const maxID = Math.max(...this.products.map((product) => product.id));
-            
             // Incrementar el contador en 1 y devolverlo como el próximo ID
             this.counter = maxID + 1;
         }
@@ -140,23 +126,25 @@ class ProductManager {
     
     //Método para borrar uno de los productos
     deleteProduct = async(id) =>{
-        const product = await this.searchById(id);
-        if(product){
-            this.products = await this.getProductsArray();
+        try{
+            const product = await this.getProductById(id);
+            if(typeof product === "string") return console.log(product);
+
+            this.products = await this.getProducts();
             //Buscamos en que indice el id coincide
             const indice = this.products.findIndex((objeto) => objeto.id === id);
 
             for (const key in product) {
-                if (key !== 'id') {
-                    product[key] = '';
-                }
-            }      
-            
+                if (key !== 'id') product[key] = '';
+            }
+
             this.products[indice] = product;
 
             const data = JSON.stringify(this.products, null, "\t");
             await fs.promises.writeFile(this.path, data, "utf-8");
-        }else{console.log(`No hay un producto con el número de ID ${id}.`)}
+        }catch(error) {
+            console.log("Hubo un error al intentar eliminar el producto ", error);
+        }
     }
 }
 
@@ -164,13 +152,13 @@ class ProductManager {
 /*Consignas
 DESAFÍO ENTREGABLE - PROCESO DE TESTING*/
 
-//1_Se creará una instancia de la clase “ProductManager”
-const manager = new ProductManager();
+//1_Se creará una instancia de la clase “ProductManager” Atención!!! se debe colocar la ruta.
+const manager = new ProductManager("./database.json");
 
 //Se crea una función asincronica que siga el orden necesitado
 (async () => {
     //2_Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
-    await manager.getProducts();
+    console.log(await manager.getProducts());
 
     //3_Se llamará al método “addProduct” con los campos:
     //title: “producto prueba”
@@ -179,32 +167,25 @@ const manager = new ProductManager();
     //thumbnail:”Sin imagen”
     //code:”abc123”,
     //stock:25
-    //manager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
+    await manager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
+    await manager.addProduct("Zapatillas deportivas", "Zapatillas ideales para hacer ejercicio", 29.99, "zapatillas.jpg", "SKU456",30);
+    await manager.addProduct("Bolso de cuero", "Bolso elegante de cuero genuino", 39.99, "bolso.jpg", "SKU789", 20);
+
     //4_El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
-    await manager.addProduct("Camiseta de algodón", "Camiseta cómoda y transpirable", 19.99, "camiseta.jpg", "SKU123",50);
-    await manager.addProduct("Camiseta de algodón", "Camiseta cómoda y transpirable", 19.99, "camiseta.jpg", "SKU123",50);
-
     //5_Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
-    await manager.getProducts();
+    console.log(await manager.getProducts());
+    await manager.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
 
+    
     //6_Se llamará al método “getProductById” y se corroborará que devuelva el producto con el id especificado, en caso de no existir, debe arrojar un error.
     await manager.getProductById(0);
-
+    
     //7	Se llamará al método “updateProduct” y se intentará cambiar un campo de algún producto, se evaluará que no se elimine el id y que sí se haya hecho la actualización.
-    await manager.updateProduct(0, "title", "150 pokemon");
-
+    await manager.updateProduct(2, "title", "150 pokemon");
+    console.log(await manager.getProducts());
+    
     //8 Se llamará al método “deleteProduct”, se evaluará que realmente se elimine el producto o que arroje un error en caso de no existir.
     await manager.deleteProduct(2);
+    console.log(await manager.getProducts());
 
-    //Más productos de prueba
-    // await manager.addProduct("Zapatillas deportivas", "Zapatillas ideales para hacer ejercicio", 29.99, "zapatillas.jpg", "SKU456",30);
-    // await manager.addProduct("Bolso de cuero", "Bolso elegante de cuero genuino", 39.99, "bolso.jpg", "SKU789", 20);
-    // await manager.addProduct("Tablet Android", "Tablet con sistema operativo Android", 199.99, "tablet.jpg", "SKU012", 10);
-    // await manager.addProduct("Cámara digital", "Cámara de alta resolución para fotografía", 299.99, "camara.jpg", "SKU345", 5);
-    // await manager.getProductById(3);
-    // await manager.addProduct("h", "h", 0.99, "h", "h", 5);
-    // await manager.addProduct("a", "a", 0.99, "a", "a", 5);
-    // await manager.addProduct("hola", "hola", 0.99, "hola", "hola", 5);
-    
-    // await manager.getProducts();
 })();
