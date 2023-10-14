dbd8fe1b0223121a7cadc8cd8c26c2694a8328c3
dariomiguel <101842755+dariomiguel@users.noreply.github.com>
2023-10-14 19:40:30 -0300

    Entrega Desafio Clase 06

--- src/app.js
+++ src/app.js
@@ -3,14 +3,34 @@ import ProductManager from "./ProductManager.js";
 
 const app = express();
 
-app.get("/", (req, res) => res.json("OK!"));
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", async(req, res) => res.json("OK!"));
 app.get("/products", async(req, res) => {
+    const limit = req.query.limit;
 
-    const productManager = new ProductManager("./database");
+    const manager = new ProductManager("./database");
+    let productos = await manager.getProducts();
 
-    const productos = await productManager.getProducts();
+    if (limit) {
+        const limitNumber = parseInt(limit, 10);
+        if (!isNaN(limitNumber) && limitNumber >= 0) {
+            productos = productos.slice(0, limitNumber);
+        }
+    }
     
-    res.json( { productos } )
+    res.json( { Productos: productos } )
 })
 
-app.listen(8080)
+
+app.get("/products/:pid", async(req, res) => {
+
+    const manager = new ProductManager("./database");
+    const productPorId = await manager.getProductById(req.params.pid);
+    
+    if(typeof productPorId === "string") return res.json( { Error: productPorId } );
+
+    res.json( { productPorId } )
+} )
+
+app.listen(8080, () => console.log("En línea..."));
